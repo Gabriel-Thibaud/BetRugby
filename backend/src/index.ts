9fd5f86188c793bb21e532e159516f50d813f18b
Gabriel-Thibaud <47009795+Gabriel-Thibaud@users.noreply.github.com>
2025-08-14 19:27:40 +0200

    Automate getting upcoming games (#11)
    * add gameService, add a cron to fetch upcoming games
    
    * add game service and route and endpoint to get upcoming games
    
    * add some typing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,9 @@ import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.route";
 import leagueRoutes from "./routes/league.route";
 import betRoutes from "./routes/bet.route";
+import gameRoutes from "./routes/game.route"
 import dotenv from "dotenv";
+import { startGameCronJobs } from "./cron/gameCron";
 
 dotenv.config();
 
@@ -15,11 +17,13 @@ app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 app.use('/api/user', userRoutes);
 app.use('/api/league', leagueRoutes);
-app.use('/api/bet', betRoutes)
+app.use('/api/bet', betRoutes);
+app.use('/api/game', gameRoutes)
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
+  startGameCronJobs();
 });
 
 
